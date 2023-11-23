ff72803167b18c2b6df01e518f4a7d3f6e749f65
nelabiernat <60930527+nelabiernat@users.noreply.github.com>
2023-11-23 17:12:07 +0100

    added API Sample

---
+++ samples/api/script.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  document.getElementById('zoomBtn').addEventListener('click', () => {
+
+    let showTeaser = true
+    let teaserZoomPercent = 10
+
+    let instance = new $FSI.Viewer('zoomEle', {
+      dir: 'images/samples/Shoe/View2',
+      debug: false,
+      plugins: 'resize,fullScreen,autoSpin',
+      skin: 'example',
+      width: '640',
+      height: '427',
+      initialView:'1,13',
+      // listen for finished loading FSI Viewer and becomes interactive
+      onReady: () => {
+        // show FSI Viewer instance and hide image
+        document.getElementById('zoomEle').style.visibility = 'visible'
+        document.getElementById('zoomImg').style.display = 'none'
+        document.getElementById('zoomBtn').style.display = 'none'
+
+        if (showTeaser) {
+          setTimeout(() => {
+
+            instance.setZoom(teaserZoomPercent, true, true)
+          }, 500)
+        }
+      },
+      // listen when zoom is finished
+      onViewChanged: (view) => {
+        if (showTeaser) {
+          showTeaser = false
+          setTimeout(() => {
+            // reset viewer - the user can interact with the UI
+            instance.resetView()
+          }, 800)
+          setTimeout(() => {
+            // reset viewer - the user can interact with the UI
+            instance.startAutoSpin()
+          }, 1500)
+        }
+      },
+    })
+
+    instance.start()
+
+  })
+
+})
